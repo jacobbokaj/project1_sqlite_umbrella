8618a1e8165a9dd1e34f780a21bde06e77b22159
Jacob <jacobvestergaard@hotmail.dk>
2023-09-26 10:19:36 +0200

    queries and sqlite

--- apps/project1_sqlite/lib/repo.ex
+++ apps/project1_sqlite/lib/repo.ex
@@ -3,4 +3,5 @@ defmodule Project1Sqlite.Repo do
     otp_app: :project1_sqlite,
     adapter: Ecto.Adapters.SQLite3
     #Project1Sqlite.Repo.all(Project1Sqlite.Furniture)
+    #Ecto.Adapters.SQL.query(Project1Sqlite.Repo, "")
 end
