f9db92e0dc706eebeb700b557a850896218a6d63
Jacob <jacobvestergaard@hotmail.dk>
2023-10-02 15:49:54 +0200

    response_car_list made to comment

--- apps/project1_sqlite_web/lib/project1_sqlite_web/live/cars_delete_component.ex
+++ apps/project1_sqlite_web/lib/project1_sqlite_web/live/cars_delete_component.ex
@@ -21,7 +21,7 @@ defmodule Project1SqliteWeb.CarsDeleteComponent do
   end
 
   def handle_event("delete_cars",%{"car_id" => car_id}, socket) do
-    response_car_list =
+    #response_car_list =
     socket = assign(socket,car_id: "")
     {:noreply,socket}
   end
