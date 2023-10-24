cf05f89f22ef8b8a1cdfd3cb1680565cb08836c4
Jacob <jacobvestergaard@hotmail.dk>
2023-10-24 10:15:15 +0200

    forkert branch update

--- apps/project1_sqlite/lib/database_queries/cars_queries.ex
+++ apps/project1_sqlite/lib/database_queries/cars_queries.ex
@@ -1,18 +1,33 @@
 defmodule Project1Sqlite.CarsQueries do
-
   def select_cars() do
     Ecto.Adapters.SQL.query(Project1Sqlite.Repo, "SELECT * FROM cars")
   end
 
-  def select_cars_where(colum,value) do
-    Ecto.Adapters.SQL.query(Project1Sqlite.Repo,"SELECT * FROM cars WHERE #{colum} = '#{value}'")
+  def select_cars_where(condition, condition_value) do
+    Ecto.Adapters.SQL.query(
+      Project1Sqlite.Repo,
+      "SELECT * FROM cars WHERE #{condition} = '#{condition_value}'"
+    )
   end
 
-  def insert_car(id,brand,model,year,price) do
-    Ecto.Adapters.SQL.query(Project1Sqlite.Repo, "INSERT INTO cars (id,brand,model,year,price) VALUES(#{id},'#{brand}','#{model}',#{year},#{price})")
+  def insert_car(id, brand, model, year, price) do
+    Ecto.Adapters.SQL.query(
+      Project1Sqlite.Repo,
+      "INSERT INTO cars (id,brand,model,year,price) VALUES(#{id},'#{brand}','#{model}',#{year},#{price})"
+    )
   end
 
-  def delete_cars_where(colum,value) do
-    Ecto.Adapters.SQL.query(Project1Sqlite.Repo,"DELETE FROM cars WHERE #{colum} = #{value}")
+  def delete_cars_where(condition, condition_value) do
+    Ecto.Adapters.SQL.query(
+      Project1Sqlite.Repo,
+      "DELETE FROM cars WHERE #{condition} = #{condition_value}"
+    )
+  end
+
+  def update_cars_where(condition, condition_value, column, value) do
+    Ecto.Adapter.SQL.query(
+      Project1Sqlite.Repo,
+      "UPDATE cars SET #{column} = #{value} WHERE #{condition} = #{condition_value}"
+    )
   end
 end
---
+++ apps/project1_sqlite_web/lib/project1_sqlite_web/live/cars_update_component.ex
@@ -0,0 +1,15 @@
+defmodule Project1SqliteWeb.CarsUpdateComponent do
+  def car_update(
+        table_name,
+        car_id,
+        update_id,
+        update_brand,
+        update_model,
+        update_year,
+        update_price
+      ) do
+  end
+
+  def cars_filter_ids() do
+  end
+end
