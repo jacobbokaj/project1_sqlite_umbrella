4df42de77217cf69898037981c9549d9a95e9f68
Jacob <jacobvestergaard@hotmail.dk>
2023-10-24 09:47:28 +0200

    ww

--- apps/project1_sqlite_web/lib/project1_sqlite_web/live/cars_delete_component.ex
+++ apps/project1_sqlite_web/lib/project1_sqlite_web/live/cars_delete_component.ex
@@ -6,25 +6,22 @@ defmodule Project1SqliteWeb.CarsDeleteComponent do
   alias Project1Sqlite.CarsContext
 
   attr(:id, :integer, required: true)
+
   def render(assigns) do
     ~H"""
     <div class="box-border h-32 w-41 p-2 border-4 ...">
-
       <form phx-submit="delete_cars" phx-target={@myself}>
-
-        <CarsDeleteComponents.car_id_input name="car_id"/>
-        <ButtonComponents.btn_simple type="submit" btn_text="Delete car"/>
-
+        <CarsDeleteComponents.car_id_input name="car_id" />
+        <ButtonComponents.btn_simple type="submit" btn_text="Delete car" />
       </form>
     </div>
     """
   end
 
-  def handle_event("delete_cars",%{"car_id" => car_id}, socket) do
-    response_car_list = CarsContext.delete_cars_with_condition("id",car_id)
-    socket = assign(socket,car_id: "")
-    {:noreply,socket}
+  def handle_event("delete_cars", %{"car_id" => car_id}, socket) do
+    CarsContext.delete_cars_with_condition("id", car_id)
+
+    socket = assign(socket, car_id: "")
+    {:noreply, socket}
   end
-
-
 end
